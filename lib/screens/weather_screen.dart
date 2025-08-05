faf46261626fb15c36ca6af89170b1dbf073dcd5
WhoisBsa <mbscbsjbs@gmail.com>
2025-08-04 22:03:47 -0300

    ft: request user location

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:open_weather/services/location_service.dart';
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   final VoidCallback onLogout;
 
   const WeatherScreen({super.key, required this.onLogout});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  Position? _position;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadLocation();
+  }
+
+  Future<void> _loadLocation() async {
+    try {
+      final pos = await LocationService.determinePosition();
+      setState(() => _position = pos);
+    } catch (e) {
+      setState(() => _error = e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,17 +37,20 @@ class WeatherScreen extends StatelessWidget {
         title: const Text('Weather'),
         actions: [
           IconButton(
-            onPressed: onLogout,
+            onPressed: widget.onLogout,
             icon: const Icon(Icons.logout),
             tooltip: 'Logout',
           ),
         ],
       ),
-      body: const Center(
-        child: Text(
-          'Weather screen (TODO)',
-          style: TextStyle(fontSize: 18),
-        ),
+      body: Center(
+        child: _error != null
+            ? Text('Error: $_error', style: const TextStyle(color: Colors.red))
+            : _position != null
+            ? Text(
+                'Your location: ${_position!.latitude}, ${_position!.longitude}',
+              )
+            : const CircularProgressIndicator(),
       ),
     );
   }
